b2018593e9bda5ca8dd2a86b3a612192bf026760
Gabriel Maldonado Almendra <gabrielmaldonado.art@gmail.com>
2018-10-02 07:54:26 -0400

    Comments clean up

--- wpjm-company-profile-page.php
+++ wpjm-company-profile-page.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WPJM Company Profile Page
  * Plugin URI:  
- * Description: Adds company profile page to WP Job Manager
+ * Description: Adds a company profile page to WP Job Manager companies
  * Author:      Gabriel Maldonado
  * Author URI:  
  * Version:     0.1
@@ -23,23 +23,6 @@ if ( !class_exists( 'WP_Job_Manager' ) ) {
 } else {
 	add_action( 'single_job_listing_meta_end', 'gma_wpjmcpp_display_job_meta_data' );
 }
-
-// ### Make the company class clickable
-// https://github.com/Automattic/WP-Job-Manager/blob/914f790050a7069a2bf7db9d5b91480d6bd256be/wp-job-manager-template.php#L1014
-
-//add_action( 'the_company_name', 'linkify_me' );
-//add_action('the_job_permalink', 'linkify_me'); //this changes the job URL permalink
-//function linkify_me($str){
-	
-	//return "hello";
-	//$company_logo = the_company_logo();
-	//print_r($company_logo);
-	//return $company_logo;
-	//$company_name = '<a href="https://google.com/" class="company_url" target="_blank">' . $company_name . '</a>';
-	//print_r($company_name);
-	//return $company_name;
-	// Cannot go this way because uses wp_strip_all_tags which removes the <a> and there's no apply filters to use there. Via company tag line we cannot either because strip tags $company_tagline = esc_attr( wp_strip_all_tags( $company_tagline ) ); OPEN EH?
-//}
 
 
 function gma_wpjmcpp_display_job_meta_data() {
