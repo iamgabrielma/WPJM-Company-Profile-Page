03d9c61ae909672c142dd86c7e1720df145601b6
Gabriel Maldonado Almendra <gabrielmaldonado.art@gmail.com>
2018-10-03 10:35:12 -0400

    Comment cleanup

--- company-archive-page-template.php
+++ company-archive-page-template.php
@@ -19,14 +19,14 @@ get_header(); ?>
 				//echo $my_individual_companies[0]->slug;
 
 				// ### Get the taxonomy rewrite slug instead to compare and echo output
-				echo '<br>';
+				//echo '<br>';
 				//$post_type = get_post_type();
 				$post_type = wp_get_post_terms($post->ID, 'companies');
 				$post_type_slug = $post_type[0]->slug;
 				//$post_type_data = get_post_type_object( $post_type );
 				//$post_type_slug = $post_type_data->rewrite['slug'];
-				var_dump($post_type);
-				var_dump($post_type_slug);
+				//var_dump($post_type);
+				//var_dump($post_type_slug);
 				//var_dump($post_type_data);
 				//var_dump($post_type_slug);
 
@@ -41,17 +41,17 @@ get_header(); ?>
 		<ul>
 			<?php
 
-				$args = array(
-					'post_type' 	=> 'job_listing',
-					'taxonomy' 		=> 'companies'
-					// 'tax_query' => array(
-					// 	array(
-					// 		'taxonomy' => 'companies'
-					// 	)
-					// )
-				);
+				// $args = array(
+				// 	'post_type' 	=> 'job_listing',
+				// 	'taxonomy' 		=> 'companies'
+				// 	// 'tax_query' => array(
+				// 	// 	array(
+				// 	// 		'taxonomy' => 'companies'
+				// 	// 	)
+				// 	// )
+				// );
 
-				$posts_array = get_posts( $args );
+				// $posts_array = get_posts( $args );
 				//var_dump($posts_array);
 				//print_r( $args );
 
@@ -63,92 +63,95 @@ get_header(); ?>
 				// echo $my_individual_companies[0]->slug;
 				//var_dump($posts_array);
 
-				foreach ($posts_array as $job => $data) {
+				// foreach ($posts_array as $job => $data) {
 
-					// URL == taxonomy, display, otherwise dont
-					// if ($post_type_slug ) {
-					// 	# code...
-					// }
+				// 	// URL == taxonomy, display, otherwise dont
+				// 	// if ($post_type_slug ) {
+				// 	// 	# code...
+				// 	// }
 					 
-					 echo '<br>';
-					 echo '----------------';
-					 //echo $job['post_title'];
-					 $job_title = $data->post_title;
-					 $job_url = $data->guid;
-					 echo '<br>';
-					 print_r($job_title);
+				// 	 echo '<br>';
+				// 	 echo '----------------';
+				// 	 //echo $job['post_title'];
+				// 	 $job_title = $data->post_title;
+				// 	 $job_url = $data->guid;
+				// 	 echo '<br>';
+				// 	 print_r($job_title);
 
-					 echo "<a href='" . $job_url . "'> | Apply </a>";
-				}
+				// 	 echo "<a href='" . $job_url . "'> | Apply </a>";
+				// }
 
 				// ## WP Query + taxonomies:
 				
 				
-				$args=array(
-					'post_type' => 'job_listing',
-				    'posts_per_page' => -1, 
-				    'tag' => $post_type_slug
-				);
+				// $args=array(
+				// 	'post_type' => 'job_listing',
+				//     'posts_per_page' => -1, 
+				//     'tag' => $post_type_slug
+				// );
 
-				$the_query = new WP_Query( $args );
+				// $the_query = new WP_Query( $args );
 
-				//var_dump($the_query);
+				// //var_dump($the_query);
 				
-				echo '<br>';
-				echo '----------------';
+				// echo '<br>';
+				// echo '----------------';
 
 				// :)
 				//var_dump($the_query->query['tag']);
 
-				if ($the_query->query['tag'] == $post_type_slug) {
-					echo 'yes! Only ' . $post_type_slug . " jobs" ;
+				// if ($the_query->query['tag'] == $post_type_slug) {
+				// 	echo 'yes! Only ' . $post_type_slug . " jobs" ;
 
 
-					$args = array(
-						'post_type' 	=> 'job_listing',
-						//'taxonomy' 		=> 'companies'
-						//'tag'			=> $post_type_slug
-						'tax_query'		=> array(
-												'taxonomy' => 'companies',
-												'field' => 'slug',
-												'terms' => $post_type_slug
-											)
-					);
+				// 	$args = array(
+				// 		'post_type' 	=> 'job_listing',
+				// 		//'taxonomy' 		=> 'companies'
+				// 		//'tag'			=> $post_type_slug
+				// 		'tax_query'		=> array(
+				// 								'taxonomy' => 'companies',
+				// 								'field' => 'slug',
+				// 								'terms' => $post_type_slug
+				// 							)
+				// 	);
 
-					$posts_array = get_posts( $args );
+				// 	$posts_array = get_posts( $args );
 					
-					foreach ($posts_array as $job => $data) {
+				// 	foreach ($posts_array as $job => $data) {
  
-						 echo '<br>';
-						 echo '----------------';
-						 $job_title = $data->post_title;
-						 $job_url = $data->guid;
-						 echo '<br>';
-						 print_r($job_title);
-						 echo "<a href='" . $job_url . "'> | Apply </a>";
-					}
+				// 		 echo '<br>';
+				// 		 echo '----------------';
+				// 		 $job_title = $data->post_title;
+				// 		 $job_url = $data->guid;
+				// 		 echo '<br>';
+				// 		 print_r($job_title);
+				// 		 echo "<a href='" . $job_url . "'> | Apply </a>";
+				// 	}
 
 
-				} else {
-					echo "no";
-				}
+				// } else {
+				// 	echo "no";
+				// }
 
-				echo '<br>';
-				echo 'new_query';
-				echo '----------------';
+				// echo '<br>';
+				// echo 'new_query';
+				// echo '----------------';
 
-				$args=array(
-					'companies' => $post_type_slug
-				);
+				// $args=array(
+				// 	'companies' => $post_type_slug
+				// );
 
-				$new_query = new WP_Query( $args );
-				var_dump($new_query);
+				// $new_query = new WP_Query( $args );
+				// var_dump($new_query);
 				
-				echo '<br>';
-				echo 'get_terms';
-				echo '----------------';
-				echo '<br>';
+				// echo '<br>';
+				// echo 'get_terms';
+				// echo '----------------';
+				// echo '<br>';
 
+				/*
+				* Working output.
+				*/
 				$myposts = get_posts(
 					array(
 		                'showposts' => -1,
@@ -164,10 +167,17 @@ get_header(); ?>
             	);
 				echo '<ul>';
 
-            	foreach ($myposts as $mypost) { ?>
+				//print_r($myposts);
+            	foreach ($myposts as $mypost) { 
+
+            		$job_title = $mypost->post_title;
+            		$job_url = $mypost->guid;
+
+            		?>
                 
                 <li>
-                	<p><?php echo $mypost->post_title ?></p>
+                	
+                	<p><?php echo  $job_title . "| <a href='" . $job_url . "'> Apply </a>"; ?></p>
             	<?php 
             	}
             	echo '</ul>';
